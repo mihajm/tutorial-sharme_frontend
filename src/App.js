6c2a8139c9e11d166dfebc5ef201efd2d8c1e0ba
miham <miham@marand.si>
2022-01-08 20:03:30 +0100

    implimented login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {Routes, Route, useNavigate} from 'react-router-dom';
+import Login from './components/Login';
+import Home from './container/Home';
 
 const App = () => (
-	<div>
-		<h1 className="">Hey</h1>
-	</div>
+	<Routes>
+		<Route path="login" element={<Login />}/>
+		<Route path="/*" element={<Home />}/>
+
+	</Routes>
 );
 
 export default App;
